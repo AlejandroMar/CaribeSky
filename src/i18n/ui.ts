3aa724f0c67d25e6c1bf340414116f67d35b2544
Alejo <lawebdealejo@gmail.com>
2024-01-25 13:40:19 -0500

    adds ui.ts

---
+++ src/i18n/ui.ts
@@ -0,0 +1,29 @@
+export const languages = {
+  es: "Es",
+  en: "En",
+};
+
+export const defaultLang = "es";
+export const showDefaultLang = false;
+
+export const ui = {
+  es: {
+    "nav.home": "Inicio",
+    "nav.aboutUs": "Nosotros",
+    "nav.contact": "Contacto",
+    "nav.blog": "Blog",
+  },
+  en: {
+    "nav.home": "Home",
+    "nav.aboutUs": "About Us",
+    "nav.contact": "Contact",
+    "nav.blog": "Blog",
+  },
+} as const;
+
+export const routes = {
+  home: "",
+  aboutUs: "about-us",
+  contact: "contact",
+  blog: "blog",
+};
