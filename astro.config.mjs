efcbf965daf5a83db87700bbc614b6adcaf346c8
Alejo <lawebdealejo@gmail.com>
2024-01-25 12:51:25 -0500

    adds site name

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://yoursite.com",
   i18n: {
     defaultLocale: "es",
     locales: ["es", "en"],
